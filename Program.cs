554d4818a75ca7af214c2b82304d053e7e1b9095
Anatoly Kirillov <kirillovtolia@gmail.com>
2022-09-01 19:32:53 +0300

    Методы

---
+++ Program.cs
@@ -0,0 +1,80 @@
+﻿// Вид1
+void Method1()
+{
+    Console.WriteLine("Автор ... ");
+}
+//Method1();
+
+
+
+
+// Вид2
+void Method2(string msg)
+{
+     Console.WriteLine(msg);
+}
+//Method2(msg: "Текст сообщения");
+
+void Method21(string msg, int count)
+{
+     int i = 0;
+     while(i < count)
+     {
+        Console.WriteLine(msg);
+        i = i + 1;
+     } 
+}
+//Method21(msg: "Текст", 4);
+
+
+
+
+// Вид3
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+int year = Method3();
+//Console.WriteLine(year);
+
+
+//Вид4
+//string Method4(int count, string text)
+
+{
+//    int i = 0;
+//    string result = String.Empty;
+//    while(i < count)
+//    {
+//        result = result + text;
+//        i = i + 1;
+//    }
+//    return result;
+//}
+//string res = Method4(10, "Z");
+//Console.WriteLine(res);
+
+string Method4(int count, string text)
+
+{
+    string result = String.Empty;
+    for(int i = 0; i < count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+
+string res = Method4(10, "Z");
+//Console.WriteLine(res);}
+
+
+//ЦИКЛ В ЦИКЛЕ
+for(int i = 1; i <= 10; i++)
+{
+    for(int j = 1; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i * j}");
+    }
+    Console.WriteLine();
+}}
